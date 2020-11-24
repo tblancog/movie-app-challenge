f256ad24e839d17953cda3f1ff6e6c2110c1c67f
Tony Blanco <tblancog@gmail.com>
2020-11-24 02:03:37 -0300

    fetching data ok

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,7 +1,11 @@
 import { Movie } from '../types/Movie';
 
 export default async function fetchMovies() {
-  const resp = await fetch('https://api.themoviedb.org/4/list/1');
+  const headers = new Headers({
+    'Content-Type': 'application/json;charset=utf-8',
+    'Authorization': `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`
+  });
+  const resp = await fetch('https://api.themoviedb.org/4/list/1', { headers });
   const data = await resp.json();
   console.log(data);
 }
